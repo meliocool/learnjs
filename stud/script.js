a1b208f945b6e9e359fa1fadf01776ee751f235d
melio <dhitanimam05@gmail.com>
2024-10-22 07:26:25 +0700

    hajimaruyoo

---
+++ stud/script.js
@@ -0,0 +1,33 @@
+/*
+    ###    ###    ##   ##    ###     ####      ####   ######    ######  ######    ######  
+     ##   ## ##   ##   ##   ## ##   ##  ##    ##  ##  ##   ##   # ## #  ##   ##   # ## #  
+     ##  ##   ##  ##   ##  ##   ##  ##       ##       ##   ##     ##    ##   ##     ##    
+ #   ##  ##   ##  ### ###  ##   ##   #####   ##       ##  ###     ##    ##   ##     ##    
+##   ##  #######   #####   #######       ##  ##       #####       ##    ######      ##    
+##   ##  ##   ##    ###    ##   ##  ##   ##   ##  ##  ## ###    # ## #  ##          ##    
+ #####   ##   ##     #     ##   ##   #####     ####   ##  ###   ######  ##          ##    
+
+*/
+
+/*
+By MelioCool
+Study Reference: youtube.com/BroCode
+*/
+
+// PRINTING OR DISPLAYING TEXT
+// ========================================================================================================
+// CONSOLE LOG => like a print function in Python, it displays a text in the console tab of the browser
+console.log("Hey!");
+
+// WINDOW.ALERT => Creates a pop up window of the given text
+window.alert("Hey!");
+
+// TEXT.CONTENT => changing the text content of an html tag
+// document => Gets the entire page, getELementById => gets a html element based on Id
+// text.Content => replace/initiate a text to display for that id
+document.getElementById("myH1").textContent = "Hello";
+// ========================================================================================================
+
+// VARIABLES
+// ========================================================================================================
+// ========================================================================================================
